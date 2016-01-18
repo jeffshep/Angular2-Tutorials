ffe46bd2f37854c2e810ead17fa91f171247da3e
Jeff Shepherd <jeffshep13@gmail.com>
2016-01-18 15:58:21 +0000

    Updates to styling

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -5,14 +5,29 @@ import {Hero} from './hero';
     selector: 'my-hero-detail',
     template: `
     <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <h2> {{hero.name}} details!</h2>
+      <div><label>ID: </label>{{hero.id}}</div>
       <div>
-        <label>name: </label>
+        <label>NAME: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
     </div>
   `,
+    styles:[`
+        label {
+        color: #ff3300;
+        font-style: Verdana;}
+        h2 {
+        margin: .5em;
+        padding: .3em 0em;
+        height: 1.6em;
+        border-radius: 4px;
+        background-color: #009933;
+        color: #ffffff;
+        font-weight: bold;
+        font-style: Verdana;
+        }
+      `],
     inputs: ['hero']
 })
 export class HeroDetailComponent {
